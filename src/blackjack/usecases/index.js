58db8b7d65419438441cfbfe4fc9eb78255a13d9
Enmanuel Monteverde <enmanuelmonteverde@gmail.com>
2025-11-12 18:25:50 -0300

    Blackjack mejorado

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearDeck } from './crear-deck'; 
+export { pedirCarta } from './pedir-cartas';
+export { crearCartaHTML } from './crear-carta-html'
+export { turnoComputadora } from './turno-computadora'; 
+export { valorCarta } from './valor-carta'; 
